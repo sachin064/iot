5ee2dcbd48936dfa21085642697b7bbd4d330fa3
Pravin kumar <pravin.kumar@3ktechnologies.com>
2019-11-13 01:16:41 +0530

    elasticsearch implmetation

--- app.py
+++ app.py
@@ -1,32 +1,34 @@
 from flask import Flask
+from sample import get_records
+from elasticsearch import Elasticsearch
 
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 app = Flask(__name__)
 if __name__ == "__main__":
+    print("hi")
+
+
+    def get_device_data():
+        index = "device_data"
+        doc_type = "device"
+        query = "SELECT * from device_data LIMIT 50"
+        records = get_records(query)
+        print(records)
+        labels = ["id", "device_id", "variable", "data", "iot_data", "created_at", "updated_at", ]
+        idx = 0
+        dictr = {}
+        for record in records:
+            for i in range(len(labels)):
+                dictr[labels[i]] = record[i]
+            res = es.index(index=index, doc_type=doc_type, id=idx, body=dictr)
+            print(res)
+            idx += 1
+        return dictr
+
+
+    # res = es.get(index='index', doc_type='doctype', id=0)
+    # print(res)
+
+    data = get_device_data()
+
     app.run(debug=True)
-
-import psycopg2
-try:
-    connection = psycopg2.connect(user = "iot",
-                                  password = "iot1234",
-                                  host = "ec2-3-80-128-28.compute-1.amazonaws.com",
-                                  port = "5432",
-                                  database = "iot")
-
-    cursor = connection.cursor()
-    # Print PostgreSQL Connection properties
-    print ( connection.get_dsn_parameters(),"\n")
-
-    cursor.execute("SELECT version();")
-    record = cursor.fetchone()
-    print("You are connected to - ", record,"\n")
-    cursor.execute("SELECT name station_masters")
-
-except (Exception, psycopg2.Error) as error :
-    print ("Error while connecting to PostgreSQL", error)
-finally:
-    #closing database connection.
-        if(connection):
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
-
--- sample.py
+++ sample.py
@@ -1,24 +1,26 @@
 import psycopg2
-try:
-    connection = psycopg2.connect(user = "iot",
-                                  password = "iot1234",
-                                  host = "ec2-3-80-128-28.compute-1.amazonaws.com",
-                                  port = "5432",
-                                  database = "iot")
+
+
+def get_records(query):
+    connection = psycopg2.connect(user="iot",
+                                  password="iot1234",
+                                  host="ec2-3-80-128-28.compute-1.amazonaws.com",
+                                  port="5432",
+                                  database="iot"
+                                  )
 
     cursor = connection.cursor()
     # Print PostgreSQL Connection properties
-    print ( connection.get_dsn_parameters(),"\n")
+    print(connection.get_dsn_parameters(), "\n")
 
-    cursor.execute("SELECT version();")
-    record = cursor.fetchone()
-    print("You are connected to - ", record,"\n")
+    print(query)
+    cursor.execute(query)
+    record = cursor.fetchall()
+    print("done")
+    print("You are connected to - ", record, "\n")
+    if connection:
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
 
-except (Exception, psycopg2.Error) as error :
-    print ("Error while connecting to PostgreSQL", error)
-finally:
-    #closing database connection.
-        if(connection):
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
+    return record
